caa9e8fa448b057846066ac5a242307038b59042
Nzii3 <exeyst@gmail.com>
2022-12-04 09:26:19 -0800

    i

---
+++ helpers.py
@@ -0,0 +1,19 @@
+import discord
+from discord import User, Member, Guild, Role, Permissions
+from typing import Optional, Union
+
+def humanize_perms(perms: Permissions):
+  simplified = []
+  key_perms = ['Administrator','Kick Members', 'Ban Members', 'Manage Channels', 'Manage Guild', 'View Audit Log', 'Manage Nicknames', 'Manage Roles', 'Manage Webhooks', 'Manage Emojis', 'Manage Events', 'Manage Threads', 'Moderate Members', 'Create Public Threads', 'Create Private Threads']
+  perms = dict(perms)
+  sorted_perms = sorted(perms.keys())
+  for perm in sorted_perms:
+    if perms[perm] == False:
+      continue
+    a = perm.replace("_", " ")
+    w = a.split()
+    r = " ".join(f"{_w.capitalize()}" for _w in w)
+    if r not in key_perms:
+      continue
+    simplified.append(r)
+  return simplified
